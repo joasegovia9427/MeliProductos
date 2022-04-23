0e0efc9d725d30454f8bab8c732963f50e867ea8
Joaquin Segovia <joasegovia9427@gmail.com>
2022-04-22 23:03:51 -0300

    Inicio del proyecto... fundacion

---
+++ MeliProductos/MeliProductos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MeliProductos
+//
+//  Created by Joaquin Segovia on 22/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MeliProductos/MeliProductos/MeliProductosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeliProductosApp.swift
+//  MeliProductos
+//
+//  Created by Joaquin Segovia on 22/4/22.
+//
+
+import SwiftUI
+
+@main
+struct MeliProductosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
